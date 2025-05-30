2ab96eca266e2965add1c49169479e057dae6a50
Devansh23dayama <143751642+Devansh23dayama@users.noreply.github.com>
2025-05-30 15:42:01 +0530

    switch

---
+++ JS/if_else.js
@@ -0,0 +1,22 @@
+let percentage= 32;
+ 
+
+if(percentage>=80 && percentage<=100){
+    console.log(" grade A")
+}
+
+else if(percentage>=70 && percentage<=79){
+    console.log(" grade B")
+}
+else if(percentage>=60 && percentage<=69){
+    console.log(" grade C")
+}
+else if(percentage>=50 && percentage<=59){
+    console.log(" grade D")
+}
+else if(percentage>=33 && percentage<=49){
+    console.log("Grade E")
+}
+else{
+    console.log("Fail")
+}
